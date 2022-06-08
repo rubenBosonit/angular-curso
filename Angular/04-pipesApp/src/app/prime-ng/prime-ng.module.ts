27968f60e4ac70536e6f232c5ccd0955c4f2c080
Rubén Plazas <ruben.plazas@bosonit.com>
2022-06-08 12:33:21 +0200

    Changes tring to fix Table PrimeNg Component

--- Angular/04-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ Angular/04-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -4,9 +4,8 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
-import { TableModule } from 'primeng/table';
 import { ToolbarModule } from 'primeng/toolbar';
-
+import { TableModule } from 'primeng/table';
 @NgModule({
   exports: [
     ButtonModule,
